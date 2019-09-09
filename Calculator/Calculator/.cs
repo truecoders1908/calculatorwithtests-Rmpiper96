dc9ab765e63f8b991fd49b330980bc6c8f38c7c9
unknown <rmpiper96@gmail.com>
2019-09-09 16:12:23 -0500

    First Commit

---
+++ Calculator/Calculator/.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace Calculator
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData(1, 7, 8)]
+        [InlineData(10, 10, 20)]
+        [InlineData(99, 30, 129)]
+        [InlineData(-10, -10, -20)]
+        [InlineData(-1, 3, 2)]
+        [InlineData(0, 1, 1)]
+        [InlineData(4, 7, 11)]
+        [InlineData(-1, 0, -1)]
+        [InlineData(5, 2, 7)]
+        
+        public void Add(int number1, int number2, int expected)
+        {
+            // Arrange
+            CalculatorTests TheTest = new CalculatorTests();
+            
+            // Act
+            int actual = TheTest.Add(number1, number2);
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(1, 7, -6)]
+        [InlineData(10, 10, 0)]
+        [InlineData(99, 30, 69)]
+        [InlineData(-10, -10, 0)]
+        [InlineData(-1, 3, -4)]
+        [InlineData(0, 1, -1)]
+        [InlineData(4, 7, -3)]
+        [InlineData(-1, 0, -1)]
+        [InlineData(5, 2, 3)]
+
+        public void Subtract(int minuend, int subtraend, int expected)
+        {
+            // Arrange
+            CalculatorTests TheTest = new CalculatorTests();
+
+            // Act
+            int actual = TheTest.Subtract(minuend, subtraend);
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public int Add(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+        public int Subtract(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+        public int multiplyTheNumbers(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+        public int divideTheNumbers(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+        public int factorializeTheNumbers(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
